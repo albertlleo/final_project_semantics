115f1765026bbd29db7d0e274a8b2386071d2f6c
Albert <albert.lleo3@gmail.com>
2019-11-13 16:55:19 +0100

    first commit

---
+++ final_project.py
@@ -0,0 +1,45 @@
+import xmltodict
+from textwrap import wrap
+import numpy as np
+
+with open('bnc_small.xml') as fd:
+    doc = xmltodict.parse(fd.read())
+    dict_sentences={}
+    dict_final={}
+    a=[]
+    array=[]
+    array2=[]
+
+    for i in range(1,len(doc['text']['s'])):
+        a.append(doc['text']['s'][i].split('\s'))
+        dict_sentences[i] = (doc['text']['s'][i].split('\s'))
+        # for ele in dict_sentences[i]:
+        #       array[i]=ele.split("\t")
+        # for ab in array:
+        #     array2.append(ab.split("\n"))
+
+        # aa = [i.split('\t', 1)[0] for i in dict_sentences[i]]
+        # dict_final[str(aa)]=(dict_sentences[i])
+
+print("")
+value = []
+
+array= [ele.split("\t") for ele in dict_sentences[i]]
+for ele in dict_sentences[1]:
+    array=ele.split("\t")
+    for i in array:
+        if "\n"  in i:
+            value.append(i)
+
+for 
+
+print(array)
+print(value)
+# print(dict_final)
+
+
+
+
+
+
+
